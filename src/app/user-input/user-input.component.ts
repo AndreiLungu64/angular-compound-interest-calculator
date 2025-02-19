3d7a8d76c32a7d7e75966b1275f2fdd17bbb4c4e
AndreiLungu64 <andreilungu777@yahoo.ro>
2025-02-19 18:38:19 +0200

    added floating effect to the header image

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SubmittedData } from './user-input.types';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  initialInvestment = signal('');
+  annualInvestment = signal('');
+  expectedReturn = signal('');
+  duration = signal('');
+  submittedData = output<SubmittedData>();
 
+  onSubmit() {
+    this.submittedData.emit({
+      initialInvestment: this.initialInvestment(),
+      annualInvestment: this.annualInvestment(),
+      expectedReturn: this.expectedReturn(),
+      duration: this.duration(),
+    });
+  }
 }
